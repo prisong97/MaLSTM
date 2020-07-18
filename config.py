11e578ed186926074511fac244631ce9ad0291a9
Priscilla <prisong97@yahoo.com.sg>
2020-07-18 22:58:58 +0800

    added config files

---
+++ config.py
@@ -0,0 +1,23 @@
+from environs import Env
+import os
+from os.path import join, exists
+
+env = Env()
+env.read_env("MaLSTM.conf", recurse=False)
+
+# model configuration
+OUTPUT_UNITS = env.int("OUTPUT_UNITS")
+MAXLEN = env.int("MAXLEN")
+NO_OF_UNIQUE_WORDS = env.int("NO_OF_UNIQUE_WORDS")
+EMBED_DIM = env.int("EMBED_DIM")
+BATCH_SIZE = env.int("BATCH_SIZE")
+LEARNING_RATE = env.float("LEARNING_RATE")
+NO_OF_ITERATIONS = env.int("NO_OF_ITERATIONS")
+SEED = env.int("SEED")
+FOLDER = env.str("ROOT_LOGDIR")
+
+if not exists(join(os.getcwd(), FOLDER)):
+    os.mkdir(join(os.getcwd(), FOLDER))
+ROOT_LOGDIR = join(os.getcwd(), FOLDER)
+
+
