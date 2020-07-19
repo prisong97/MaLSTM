9870f3b477bc5f137310dab0340b7966c1945334
Priscilla <prisong97@yahoo.com.sg>
2020-07-19 13:41:27 +0800

    added comment

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from config import OUTPUT_UNITS, MAXLEN, NO_OF_UNIQUE_WORDS, EMBED_DIM, HOLDOUT_
 from process_data import process_df
 
 if __name__ == "__main__":
-     import data in correct form
+    #  import data in correct form
     X_sentence_1, X_sentence_2, X_sentence_1_length, X_sentence_2_length, y_data = process_df(MAXLEN)
     #  train the model
     model = MaLSTM(OUTPUT_UNITS, MAXLEN, NO_OF_UNIQUE_WORDS, EMBED_DIM)
