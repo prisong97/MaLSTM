03b8e02607759601afccb5b6d7b026498c353bd9
Priscilla <prisong97@yahoo.com.sg>
2020-07-19 13:30:07 +0800

    added support for holdout ratio

--- train.py
+++ train.py
@@ -1,12 +1,11 @@
-import pandas as pd
-from MaLSTM import MaLSTM
-from config import OUTPUT_UNITS, MAXLEN, NO_OF_UNIQUE_WORDS, EMBED_DIM, BATCH_SIZE, LEARNING_RATE, NO_OF_ITERATIONS, SEED, ROOT_LOGDIR
+# from MaLSTM import MaLSTM
+from config import OUTPUT_UNITS, MAXLEN, NO_OF_UNIQUE_WORDS, EMBED_DIM, HOLDOUT_RATIO, BATCH_SIZE, LEARNING_RATE, NO_OF_ITERATIONS, SEED, ROOT_LOGDIR
 from process_data import process_df
 
-if __name__ == __main__:
-    #  import data in correct form
-    X_sentence_1, X_sentence_2, X_sentence_1_length, X_sentence_2_length, y_data = process_df()
+if __name__ == "__main__":
+     import data in correct form
+    X_sentence_1, X_sentence_2, X_sentence_1_length, X_sentence_2_length, y_data = process_df(MAXLEN)
     #  train the model
     model = MaLSTM(OUTPUT_UNITS, MAXLEN, NO_OF_UNIQUE_WORDS, EMBED_DIM)
     model.train(X_sentence_1, X_sentence_2, X_sentence_1_length, X_sentence_2_length, y_data,\
-        BATCH_SIZE, LEARNING_RATE, NO_OF_ITERATIONS, SEED, ROOT_LOGDIR)
+        HOLDOUT_RATIO, BATCH_SIZE, LEARNING_RATE, NO_OF_ITERATIONS, SEED, ROOT_LOGDIR)
